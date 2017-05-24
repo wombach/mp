ea0610062fa8818a3c47240548e9000466d96ca2
AN332496 <andreas.wombacher@wipro.com>
2017-05-24 23:21:51 +0200

    added a json test

--- src/test/java/org/opengroup/xsd/archimate/_3/MarshalUnmarshalExampleFileTest.java
+++ src/test/java/org/opengroup/xsd/archimate/_3/MarshalUnmarshalExampleFileTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -17,6 +18,8 @@ import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.transform.stream.StreamSource;
 
+import org.eclipse.persistence.jaxb.MarshallerProperties;
+import org.eclipse.persistence.jaxb.UnmarshallerProperties;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -44,11 +47,11 @@ public class MarshalUnmarshalExampleFileTest {
 		jaxbContext =  JAXBContext.newInstance(ModelType.class);
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		FileWriter out = new FileWriter( "Main5_out.xml");
+		FileWriter out = new FileWriter( "demo_archimate3_result.xml");
 		marshaller.marshal(model, out);
 		out.flush();
 		out.close();
-		File file2 = new File("Main5_out.xml");
+		File file2 = new File("demo_archimate3_result.xml");
 		//		
 		//		Diff diff = DiffBuilder.compare(Input.fromFile(file))
 		//				   .withTest(Input.fromFile(file2))
@@ -60,6 +63,48 @@ public class MarshalUnmarshalExampleFileTest {
 		//		}
 		assertThat(file, CompareMatcher.isSimilarTo(file2).ignoreComments().ignoreWhitespace());
 
+	}
+
+	@Test
+	public void givenJsonFile_unmarshalJson_marshalJson_expectFileEqualsResult() throws JAXBException, IOException{
+		JAXBContext jaxbContext =  JAXBContext.newInstance(ModelType.class);
+
+		Map<String, String> namespaces = new HashMap<String, String>();
+		namespaces.put("http://www.opengroup.org/xsd/archimate/3.0/", "");
+		namespaces.put("http://www.opengroup.org/xsd/archimate/3.0/", "ar3");
+		namespaces.put("http://www.w3.org/2001/XMLSchema-instance", "xsi");
+
+		File file = new File("demo_archimate3.json");
+		StreamSource source = new StreamSource(file);
+		Unmarshaller unmarshaller2 = jaxbContext.createUnmarshaller();
+		unmarshaller2.setProperty(UnmarshallerProperties.JSON_NAMESPACE_PREFIX_MAPPER, namespaces);
+		unmarshaller2.setProperty(UnmarshallerProperties.JSON_NAMESPACE_SEPARATOR, '_');
+		unmarshaller2.setProperty(UnmarshallerProperties.MEDIA_TYPE, "application/json");
+		unmarshaller2.setProperty(UnmarshallerProperties.JSON_ATTRIBUTE_PREFIX, "@") ;
+		unmarshaller2.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, true);
+		JAXBElement<ModelType> result = unmarshaller2.unmarshal(source, ModelType.class);
+		ModelType model = (ModelType) result.getValue();
+
+//		jaxbContext =  JAXBContext.newInstance(ModelType.class);
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		marshaller.setProperty(MarshallerProperties.NAMESPACE_PREFIX_MAPPER, namespaces);
+		marshaller.setProperty(MarshallerProperties.JSON_NAMESPACE_SEPARATOR, '_');
+		marshaller.setProperty(MarshallerProperties.JSON_ATTRIBUTE_PREFIX, "@") ;
+		marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+		marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, true);
+		FileWriter out = new FileWriter( "demo_archimate3_result.json");
+		marshaller.marshal(model, out);
+		out.flush();
+		out.close();
+		File file2 = new File("demo_archimate3_result.json");
+		String content = new Scanner(file).useDelimiter("\\Z").next();
+		String content2 = new Scanner(file2).useDelimiter("\\Z").next();
+		try {
+			JSONAssert.assertEquals(content, content2, false);
+		} catch (JSONException e) {
+			fail("Should not have thrown any exception");
+		}
 	}
 
 	@Test
